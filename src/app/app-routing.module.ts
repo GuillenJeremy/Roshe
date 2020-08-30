d51eab3ae8020b0b5477a2112d80183b358ef68a
Jergui <guillenjeremy26@gmail.com>
2020-08-30 14:14:07 -0600

    fixing layout issue and adding routing module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {ProfileComponent} from './pages/profile/profile.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'profile'
+  },
+  {
+    path: 'profile',
+    component: ProfileComponent,
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,14 @@ import { AppComponent } from './app.component';
 import {EmergencyButtonComponent} from './components/emergency-button/emergency-button.component';
 import {NavbarComponent} from './components/common/navbar/navbar.component';
 import { HomeComponent } from './pages/home/home.component';
+import { ProfileComponent } from './pages/profile/profile.component';
 @NgModule({
   declarations: [
     AppComponent,
     EmergencyButtonComponent,
     HomeComponent,
-    NavbarComponent
+    NavbarComponent,
+    ProfileComponent
   ],
   imports: [
     BrowserModule,
