be578de51eab7a08c729b342cdf975bc4c94f041
Jergui <guillenjeremy26@gmail.com>
2020-08-30 10:49:11 -0600

    adding home component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,12 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {EmergencyButtonComponent} from './components/emergency-button/emergency-button.component';
 import {} from './components/common/navbar/navbar.component';
+import { HomeComponent } from './pages/home/home.component';
 @NgModule({
   declarations: [
     AppComponent,
     EmergencyButtonComponent,
+    HomeComponent,
 
   ],
   imports: [
