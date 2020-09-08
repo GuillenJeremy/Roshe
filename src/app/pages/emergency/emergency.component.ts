95c4102cfa36aa7a44713fbba2469a7cad432e42
Jergui <guillenjeremy26@gmail.com>
2020-09-07 19:49:18 -0600

    confirmation modal added

--- src/app/pages/emergency/emergency.component.ts
+++ src/app/pages/emergency/emergency.component.ts
@@ -6,10 +6,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./emergency.component.scss']
 })
 export class EmergencyComponent implements OnInit {
+  showConfirmationModal: boolean = false;
+  code: number;
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  public onButtonClicked() {
+    if(!this.showConfirmationModal) {
+      this.code = Math.floor(Math.random() * (4 - 1) + 1);
+      this.showConfirmationModal = true;
+      console.log('ELEMENT CLICK', this.showConfirmationModal);
+    }
+  }
+
+  public checkCode(selectedCode: number) {
+    if(this.code === selectedCode) {
+      console.log(this.code);
+      this.closeConfirmationModal();
+    }
+  }
+
+  public closeConfirmationModal() {
+    this.showConfirmationModal = false;
   }
 
 }
